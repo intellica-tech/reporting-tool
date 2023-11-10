c322d6c2212d43c101a958bef514fb99beaf9932
omerfbolat <anekdo1@gmail.com>
2023-11-10 16:43:53 +0300

    fix: styled update

--- superset-frontend/src/components/DvtTitlePlus/dvt-title-plus.module.tsx
+++ superset-frontend/src/components/DvtTitlePlus/dvt-title-plus.module.tsx
@@ -4,17 +4,13 @@ const StyledDvtTitlePlus = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: transparent;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 700;
-  text-transform: uppercase;
 `;
 
 const DvtTitlePlusTitle = styled.p`
   font-size: 12px;
   font-style: normal;
   font-weight: 700;
+  text-transform: uppercase;
   margin: 0;
   color: ${({ theme }) => theme.colors.dvt.text.label};
 `;
