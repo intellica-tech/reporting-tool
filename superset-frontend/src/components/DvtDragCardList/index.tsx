03c7e659b79155d0962db2a9a7665ab1c0a91b4b
Muhammed BABAN <93913650+Muhammed-baban@users.noreply.github.com>
2024-01-17 16:40:30 +0300

    update

--- superset-frontend/src/components/DvtDragCardList/index.tsx
+++ superset-frontend/src/components/DvtDragCardList/index.tsx
@@ -16,24 +16,22 @@ export interface DvtDragCardListProps {
   data: CardProps[];
 }
 
-const DvtDargCardList = ({ data }: DvtDragCardListProps) => {
-  return (
-    <StyledDvtDragCardList>
-      <StyledDvtDragCardListSize>
-        Showing {data.length} of {data.length}
-      </StyledDvtDragCardListSize>
-      <StyledDvtDragCard>
-        {data.map((item, index) => (
-          <DvtDargCard
-            key={index}
-            label={item.label}
-            value={item.value}
-            icon={item.icon}
-          />
-        ))}{' '}
-      </StyledDvtDragCard>
-    </StyledDvtDragCardList>
-  );
-};
+const DvtDargCardList = ({ data }: DvtDragCardListProps) => (
+  <StyledDvtDragCardList>
+    <StyledDvtDragCardListSize>
+      Showing {data.length} of {data.length}
+    </StyledDvtDragCardListSize>
+    <StyledDvtDragCard>
+      {data.map((item, index) => (
+        <DvtDargCard
+          key={index}
+          label={item.label}
+          value={item.value}
+          icon={item.icon}
+        />
+      ))}
+    </StyledDvtDragCard>
+  </StyledDvtDragCardList>
+);
 
 export default DvtDargCardList;
