c2ccd7a433f23a7bd47b4e45bd2c4936f3f602bf
Muhammed BABAN <93913650+Muhammed-baban@users.noreply.github.com>
2024-01-18 16:10:36 +0300

    Revert "Delete superset-frontend/src/components/DvtDragCard/dvt-drag-card.module.tsx"
    This reverts commit bf101d8697a54d02ecdeb0128ee83fb715808b9a.

---
+++ superset-frontend/src/components/DvtDragCard/dvt-drag-card.module.tsx
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { styled } from '@superset-ui/core';
+
+const StyledDvtCard = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const StyledDvtCardCard = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  width: 202px;
+  height: 48px;
+  border-radius: 12px;
+  background: ${({ theme }) => theme.colors.dvt.grayscale.light2};
+  margin-bottom: 12px;
+`;
+
+const StyledDvtCardIcon = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 13px;
+  color: ${({ theme }) => theme.colors.dvt.text.label};
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const StyledDvtCardLabel = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.dvt.primary.light1};
+`;
+
+export {
+  StyledDvtCard,
+  StyledDvtCardCard,
+  StyledDvtCardIcon,
+  StyledDvtCardLabel,
+};
