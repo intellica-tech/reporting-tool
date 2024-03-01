ae90fd29a633784133e10b2048fb5c61b332eebb
Ulaş Baran Tomak <ulasbaran.tomak@std.yeditepe.edu.tr>
2024-03-01 17:53:54 +0300

    fix: fixed page

--- superset-frontend/src/dvt-modal/body/rowLevelSecurity-add/index.tsx
+++ superset-frontend/src/dvt-modal/body/rowLevelSecurity-add/index.tsx
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import React, { useEffect, useState } from 'react';
 import { ModalProps } from 'src/dvt-modal';
 import useFetch from 'src/hooks/useFetch';
