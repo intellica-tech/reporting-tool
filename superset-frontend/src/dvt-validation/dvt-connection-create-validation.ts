f56c6ce2ea7871bf7123852d31e9808b89bbb11f
omerfbolat <anekdo1@gmail.com>
2024-03-05 09:00:56 +0300

    fix: new validation connection add is added

---
+++ superset-frontend/src/dvt-validation/dvt-connection-create-validation.ts
@@ -0,0 +1,39 @@
+import { t } from '@superset-ui/core';
+
+const connectionCreateValidation = {
+  host: (value: string) => {
+    const ipAddressRegex =
+      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if (!value) {
+      return t('This is a required field');
+    }
+    if (!ipAddressRegex.test(value)) {
+      return t('is not a valid IP address.');
+    }
+    return undefined;
+  },
+  port: (value: string) => {
+    const portNumber = parseInt(value, 10);
+    if (!value) {
+      return t('This is a required field');
+    }
+    if (portNumber < 0 || portNumber > 65535) {
+      return t('Port must be between 0 and 65535');
+    }
+    return undefined;
+  },
+  database_name: (value: string) => {
+    if (!value) {
+      return t('This is a required field');
+    }
+    return undefined;
+  },
+  user_name: (value: string) => {
+    if (!value) {
+      return t('This is a required field');
+    }
+    return undefined;
+  },
+};
+
+export default connectionCreateValidation;
