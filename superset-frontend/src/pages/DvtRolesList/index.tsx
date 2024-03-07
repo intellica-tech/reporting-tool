7dbb6445741002607f259856dd8ba33f62859b3e
omerfbolat <anekdo1@gmail.com>
2024-03-07 16:56:33 +0300

    fix: list roles changes fixed

--- superset-frontend/src/pages/DvtRolesList/index.tsx
+++ superset-frontend/src/pages/DvtRolesList/index.tsx
@@ -1,5 +1,24 @@
-import { t } from '@superset-ui/core';
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import React, { useEffect, useState } from 'react';
+import { t } from '@superset-ui/core';
 import DvtButton from 'src/components/DvtButton';
 import DvtTable from 'src/components/DvtTable';
 
@@ -60,12 +79,13 @@ function DvtListRoles() {
     },
   ];
   return (
-    <div>
+    <>
       <div
         style={{
           display: 'flex',
           flexDirection: 'row',
           justifyContent: 'space-between',
+          marginBottom: 30,
         }}
       >
         <DvtButton
@@ -75,17 +95,9 @@ function DvtListRoles() {
           colour="primary"
           typeColour="powder"
         />
-        <DvtButton
-          onClick={() => {}}
-          bold
-          icon="dvt-linear_arrow"
-          colour="primary"
-          typeColour="powder"
-        />
       </div>
-
       <DvtTable data={data} header={listRolesHeaderData} />
-    </div>
+    </>
   );
 }
 
