77e5c8f24375c58cda651dc1207bc86692cb21ce
Ulaş Baran Tomak <ulasbaran.tomak@std.yeditepe.edu.tr>
2024-03-19 16:55:26 +0300

    fix: dvt199 fixed dashboard

--- superset-frontend/src/dvt-modal/body/dashboard-edit/index.tsx
+++ superset-frontend/src/dvt-modal/body/dashboard-edit/index.tsx
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import React, { useEffect, useState } from 'react';
 import { t } from '@superset-ui/core';
 import { ModalProps } from 'src/dvt-modal';
@@ -26,7 +44,7 @@ const DvtDashboardEdit = ({ meta, onClose }: ModalProps) => {
     certificationDetails: '',
   });
   const [dashboardApi, setDashboardApi] = useState<string>('');
-  const [jsonValue, setJsonValue] = useState<any | null>('');
+  const [jsonValue, setJsonValue] = useState<string>('');
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const dashboardItemApi = useFetch({ url: `dashboard/${meta.id}` });
@@ -62,7 +80,7 @@ const DvtDashboardEdit = ({ meta, onClose }: ModalProps) => {
       dashboard_title: values.title,
       owners: values.owners,
       slug: values.urlSlug,
-      json_metadata: JSON.stringify(jsonValue),
+      json_metadata: jsonValue,
     },
   });
 
@@ -83,15 +101,22 @@ const DvtDashboardEdit = ({ meta, onClose }: ModalProps) => {
     }
   }, [onClose, updateDashboardData]);
 
-  const defaultJsonValue = `{
-    "color_scheme": "${values.colorSchema?.label || ''}",
-    "label_colors": {},
-    "timed_refresh_immune_slices": [],
-    "expanded_slices": {},
-    "refresh_frequency": 0,
-    "cross_filters_enabled": true,
-    "shared_label_colors": {},
-  }`;
+  useEffect(() => {
+    const formattedJson = `{
+      "color_scheme": "${values.colorSchema?.id || ''}",
+      "label_colors": {},
+      "shared_label_colors": {},
+      "color_scheme_domain": [
+        ${
+          values.colorSchema?.colors
+            ?.map((color: string) => `"${color}"`)
+            .join(',\n    ') || ''
+        }
+      ]
+    }`;
+
+    setJsonValue(formattedJson);
+  }, [values.colorSchema]);
 
   const handleOnChange = (key: string, value: any) => {
     setValues((state: any) => ({ ...state, [key]: value }));
@@ -149,7 +174,7 @@ const DvtDashboardEdit = ({ meta, onClose }: ModalProps) => {
           </StyledDashboardEditInput>
         </StyledDashboardEditGroup>
         <DvtCollapse label="ADVANCED" isOpen={isOpen} setIsOpen={setIsOpen}>
-          <DvtJsonEditor value={defaultJsonValue} onChange={setJsonValue} />
+          <DvtJsonEditor value={jsonValue} onChange={setJsonValue} />
         </DvtCollapse>
       </StyledDashboardEditBody>
     </StyledDashboardEdit>
