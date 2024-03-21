d05094cf2bab619f0470bee5cf702db5f6c0de81
Ulaş Baran Tomak <ulasbaran.tomak@std.yeditepe.edu.tr>
2024-03-21 12:37:57 +0300

    fix: fixed

--- superset-frontend/src/pages/DvtSavedQuery/index.tsx
+++ superset-frontend/src/pages/DvtSavedQuery/index.tsx
@@ -16,11 +16,11 @@ import DvtPagination from 'src/components/DvtPagination';
 import DvtTable, { DvtTableSortProps } from 'src/components/DvtTable';
 import useFetch from 'src/dvt-hooks/useFetch';
 import { fetchQueryParamsSearch } from 'src/dvt-utils/fetch-query-params';
+import DvtIconDataLabel from 'src/components/DvtIconDataLabel';
 import {
   SavedQueryPagination,
   StyledEmptyAlerts,
 } from './dvt-saved-query.module';
-import DvtIconDataLabel from 'src/components/DvtIconDataLabel';
 
 function DvtSavedQuery() {
   const dispatch = useDispatch();
