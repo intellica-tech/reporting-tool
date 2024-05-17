9cbae5403706b86e187d573a9c4ca85f60484a40
mx <contact@erayerturk.com>
2024-05-17 15:06:07 +0300

    data-process - create endpoints vol4

--- superset/dvt_dataprocess/helpers.py
+++ superset/dvt_dataprocess/helpers.py
@@ -23,9 +23,9 @@ def write_dataframe_to_table(df, engine, table_name):
 
 
 def detect_outliers_boxplot(data):
-            q1 = data.quantile(0.25)
-            q3 = data.quantile(0.75)
-            iqr = q3 - q1
-            lower_bound = q1 - 1.5 * iqr
-            upper_bound = q3 + 1.5 * iqr
-            return data[(data < lower_bound) | (data > upper_bound)]
+    q1 = data.quantile(0.25)
+    q3 = data.quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    return data[(data < lower_bound) | (data > upper_bound)]
